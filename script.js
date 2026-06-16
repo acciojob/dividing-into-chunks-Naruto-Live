86adb5c72a6d001fc8da7e1d92fa1e0f8c5b9c80
Shubham Raj ( Lucky ) <sr7773879@gmail.com>
2026-06-16 16:30:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
+let sum = 0;
+let result = []
+let chunk = []
 
 const divide = (arr, n) => {
-  // Write your code here
+  for( let i = 0 ; i < arr.length; i++ ) {
+	let num = arr[i];
+	  if( sum + num <= n ) {
+		  chunk.push(num);
+		  sum += num;
+	  } else {
+		result.push(chunk);
+		  chunk = [num];
+		  sum = num;
+	  }
+  }
+	result.push(chunk);
+	return result;
 };
 
 const n = prompt("Enter n: ");
